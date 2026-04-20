2885c8807c05def04a4008ce60e2b8fe9caed076
KitaH-H <simon.krisztian.zsolt@diak.szbi-pg.hu>
2026-04-20 08:07:58 +0200

    esemenyek

--- esemenyek.sql
+++ esemenyek.sql
@@ -12,26 +12,26 @@ CREATE TABLE `emberek` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 INSERT INTO `emberek` (`nev`, `email`, `telefonszam`, `szerep`, `sorszam`) VALUES
-('Kiss Péter', 'peter1@email.hu', '+3611111111', 'user', NULL),
-('Nagy Anna', 'anna2@email.hu', '+3611111112', 'user', NULL),
-('Tóth László', 'laszlo3@email.hu', '+3611111113', 'user', NULL),
-('Szabó Éva', 'eva4@email.hu', '+3611111114', 'user', NULL),
-('Kovács Gábor', 'gabor5@email.hu', '+3611111115', 'user', NULL),
-('Molnár Zoltán', 'zoltan6@email.hu', '+3611111116', 'user', NULL),
-('Varga Júlia', 'julia7@email.hu', '+3611111117', 'user', NULL),
-('Horváth Tamás', 'tamas8@email.hu', '+3611111118', 'user', NULL),
-('Balogh Dóra', 'dora9@email.hu', '+3611111119', 'user', NULL),
-('Papp András', 'andras10@email.hu', '+3611111120', 'user', NULL),
-('Farkas Krisztina', 'kriszti11@email.hu', '+3611111121', 'user', NULL),
-('Takács Márk', 'mark12@email.hu', '+3611111122', 'user', NULL),
-('Juhász Rita', 'rita13@email.hu', '+3611111123', 'user', NULL),
-('Mészáros Bence', 'bence14@email.hu', '+3611111124', 'user', NULL),
-('Oláh Noémi', 'noemi15@email.hu', '+3611111125', 'user', NULL),
-('Simon Ádám', 'adam16@email.hu', '+3611111126', 'user', NULL),
-('Lakatos Eszter', 'eszter17@email.hu', '+3611111127', 'user', NULL),
-('Németh Dávid', 'david18@email.hu', '+3611111128', 'user', NULL),
-('Szalai Kata', 'kata19@email.hu', '+3611111129', 'user', NULL),
-('Veres Bálint', 'balint20@email.hu', '+3611111130', 'user', NULL);
+('Kiss Péter', 'peter1@email.hu', '+3611111111', 'user', AUTO_INCREMENT),
+('Nagy Anna', 'anna2@email.hu', '+3611111112', 'user',  AUTO_INCREMENT),
+('Tóth László', 'laszlo3@email.hu', '+3611111113', 'user',  AUTO_INCREMENT),
+('Szabó Éva', 'eva4@email.hu', '+3611111114', 'user',  AUTO_INCREMENT),
+('Kovács Gábor', 'gabor5@email.hu', '+3611111115', 'user',  AUTO_INCREMENT),
+('Molnár Zoltán', 'zoltan6@email.hu', '+3611111116', 'user',  AUTO_INCREMENT),
+('Varga Júlia', 'julia7@email.hu', '+3611111117', 'user',  AUTO_INCREMENT),
+('Horváth Tamás', 'tamas8@email.hu', '+3611111118', 'user',  AUTO_INCREMENT),
+('Balogh Dóra', 'dora9@email.hu', '+3611111119', 'user',  AUTO_INCREMENT),
+('Papp András', 'andras10@email.hu', '+3611111120', 'user',  AUTO_INCREMENT),
+('Farkas Krisztina', 'kriszti11@email.hu', '+3611111121', 'user',  AUTO_INCREMENT),
+('Takács Márk', 'mark12@email.hu', '+3611111122', 'user',  AUTO_INCREMENT),
+('Juhász Rita', 'rita13@email.hu', '+3611111123', 'user',  AUTO_INCREMENT),
+('Mészáros Bence', 'bence14@email.hu', '+3611111124', 'user',  AUTO_INCREMENT),
+('Oláh Noémi', 'noemi15@email.hu', '+3611111125', 'user',  AUTO_INCREMENT),
+('Simon Ádám', 'adam16@email.hu', '+3611111126', 'user',  AUTO_INCREMENT),
+('Lakatos Eszter', 'eszter17@email.hu', '+3611111127', 'user',  AUTO_INCREMENT),
+('Németh Dávid', 'david18@email.hu', '+3611111128', 'user',  AUTO_INCREMENT),
+('Szalai Kata', 'kata19@email.hu', '+3611111129', 'user',  AUTO_INCREMENT),
+('Veres Bálint', 'balint20@email.hu', '+3611111130', 'user',  AUTO_INCREMENT);
 
 CREATE TABLE `esemeny` (
   `nev` varchar(20) COLLATE utf8mb4_hungarian_ci NOT NULL,
@@ -43,11 +43,11 @@ CREATE TABLE `esemeny` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 INSERT INTO `esemeny` (`nev`, `idopont`, `id`, `leiras`, `kategoria`, `sorszam`) VALUES
-('Konferencia', '2026-05-10', 1, 'Nemzetközi tech konferencia', 'Technológia', NULL),
-('Workshop', '2026-06-15', 2, 'Haladó SQL workshop', 'Oktatás', NULL),
-('Fesztivál', '2026-07-20', 3, 'Nyári zenei fesztivál', 'Zene', NULL),
-('Találkozó', '2026-08-05', 4, 'Startup üzleti találkozó', 'Üzlet', NULL),
-('Sportnap', '2026-09-01', 5, 'Céges sportnap és versenyek', 'Sport', NULL);
+('Konferencia', '2026-05-10', 1, 'Nemzetközi tech konferencia', 'Technológia',  AUTO_INCREMENT),
+('Workshop', '2026-06-15', 2, 'Haladó SQL workshop', 'Oktatás',  AUTO_INCREMENT),
+('Fesztivál', '2026-07-20', 3, 'Nyári zenei fesztivál', 'Zene',  AUTO_INCREMENT),
+('Találkozó', '2026-08-05', 4, 'Startup üzleti találkozó', 'Üzlet',  AUTO_INCREMENT),
+('Sportnap', '2026-09-01', 5, 'Céges sportnap és versenyek', 'Sport', AUTO_INCREMENTNULL);
 
 CREATE TABLE `kapcsolo` (
   `telefonszam` varchar(15) COLLATE utf8mb4_hungarian_ci DEFAULT NULL,
@@ -57,26 +57,26 @@ CREATE TABLE `kapcsolo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 INSERT INTO `kapcsolo` (`telefonszam`, `id`, `szerep`, `sorszam`) VALUES
-('+3611111111', 1, 1, 1),
-('+3611111112', 1, 0, 2),
-('+3611111113', 1, 0, 3),
-('+3611111114', 2, 1, 4),
-('+3611111115', 2, 0, 5),
-('+3611111116', 2, 0, 6),
-('+3611111117', 3, 1, 7),
-('+3611111118', 3, 0, 8),
-('+3611111119', 3, 0, 9),
-('+3611111120', 4, 1, 10),
-('+3611111121', 4, 0, 11),
-('+3611111122', 4, 0, 12),
-('+3611111123', 5, 1, 13),
-('+3611111124', 5, 0, 14),
-('+3611111125', 5, 0, 15),
-('+3611111126', 1, 0, 16),
-('+3611111127', 2, 0, 17),
-('+3611111128', 3, 0, 18),
-('+3611111129', 4, 0, 19),
-('+3611111130', 5, 0, 20);
+('+3611111111', 1, 1,  AUTO_INCREMENT),
+('+3611111112', 1, 0,  AUTO_INCREMENT),
+('+3611111113', 1, 0,  AUTO_INCREMENT),
+('+3611111114', 2, 1,  AUTO_INCREMENT),
+('+3611111115', 2, 0,  AUTO_INCREMENT),
+('+3611111116', 2, 0,  AUTO_INCREMENT),
+('+3611111117', 3, 1,  AUTO_INCREMENT),
+('+3611111118', 3, 0,  AUTO_INCREMENT),
+('+3611111119', 3, 0,  AUTO_INCREMENT),
+('+3611111120', 4, 1,  AUTO_INCREMENT),
+('+3611111121', 4, 0,  AUTO_INCREMENT),
+('+3611111122', 4, 0,  AUTO_INCREMENT),
+('+3611111123', 5, 1,  AUTO_INCREMENT),
+('+3611111124', 5, 0,  AUTO_INCREMENT),
+('+3611111125', 5, 0,  AUTO_INCREMENT),
+('+3611111126', 1, 0,  AUTO_INCREMENT),
+('+3611111127', 2, 0,  AUTO_INCREMENT),
+('+3611111128', 3, 0,  AUTO_INCREMENT),
+('+3611111129', 4, 0,  AUTO_INCREMENT),
+('+3611111130', 5, 0,  AUTO_INCREMENT);
 
 ALTER TABLE `emberek`
   ADD PRIMARY KEY (`telefonszam`),
